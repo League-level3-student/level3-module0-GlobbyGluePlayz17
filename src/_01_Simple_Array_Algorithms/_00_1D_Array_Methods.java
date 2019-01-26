6f052fcab1da3afc4571943be196882048f4ebe7
League <league@WTS8.attlocal.net>
2019-01-26 15:45:53 -0800

    VisualArraySorter, Simple Array Algorithm (IP)

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,15 +4,23 @@ public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
-		return 0;
+		int a = 0;
+		for (int i = 0; i < values.length; i++) {
+			int b = values[i] + a;
+			b = a;
+		}
+		return a;
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-				
-		return 0;
+		double sum = 0;
+		for (int i = 0; i < values.length; i++) {
+			double b = values[i] + sum;
+			b = sum;
+		}
+		return sum/values.length;
 	}
 	
 	
@@ -20,15 +28,31 @@ public class _00_1D_Array_Methods {
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
-		return false;
+		boolean rETURN = false;
+		
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				rETURN = true;
+			} else {
+				rETURN = false;
+			}
+		}
+		return rETURN;
 	}
 	
-	//4. Complete the method so that it returns the index of the,
+	//4. Complete the method so that it returns the index of the
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
+		int RETURN = -1;
 		
-		return 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == value) {
+				RETURN = i;
+			} else {
+				RETURN = -1;
+			}
+		}
+		return RETURN;
 	}
 }
